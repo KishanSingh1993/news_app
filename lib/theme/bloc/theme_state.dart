e44a9464a5f7be1f387571c6433de0cfbf35f5a9
kishansingh <kishang.90@gmail.com>
2020-08-30 15:30:44 +0530

    new changes

---
+++ lib/theme/bloc/theme_state.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
+
+class ThemeState extends Equatable{
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+
+
+}
+
+class SelectedTheme extends ThemeState{
+
+  final ThemeType themeType;
+
+  SelectedTheme({this.themeType});
+
+  @override
+  List<Object> get props => [themeType];
+
+  @override
+  String toString() => 'Theme Selected ';
+}
+
+enum ThemeType { Light, Dark }
