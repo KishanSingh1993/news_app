32c14cb9a9518001de0b91af8e8a809750c02714
kishansingh <kishang.90@gmail.com>
2020-08-31 14:52:38 +0530

    new changes

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,5 +1,5 @@
 class Constant{
-  static const String newsApiKey = 'e98302103c954996a47a9ecacd3331ef';
+  static const String newsApiKey = 'YOUR_KEY';
   static const String baseUrl = 'https://newsapi.org/v2/';
   static const String topHeadLine = '/top-headlines';
 }
